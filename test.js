fe34cc84f41db4dbee0ef7cf81fca3be02a00532
FukeKazki <kazkichi0906@gmail.com>
2019-05-18 14:06:58 +0900

    Update: async await を使って直列処理

--- test.js
+++ test.js
@@ -10,84 +10,73 @@ const kuromoji = require('kuromoji');
 const app = express();
 //middleware: Expressの関数 req,resを呼び出すときに使う
 app.post('/webhook', line.middleware(setting.config), (req, res) => {
-  //Promise: 非同期処理 callback地獄を避ける
-  Promise
+    //Promise: 非同期処理 callback地獄を避ける
+    Promise
     //all: 非同期処理が成功した場合にcallbackする 引数は監視するオブジェクト郡(配列)  
-    .all(req.body.events.map(handleEvent))
-    //全部成功->thenを実行 resultをjson形式で戻す
-    .then((result) => res.json(result));
+        .all(req.body.events.map(handleEvent))
+        //全部成功->thenを実行 resultをjson形式で戻す
+        .then((result) => res.json(result));
 });
 
 
 const client = new line.Client(setting.config);
 const handleEvent = event => {
-  //メッセージでなかったらnullを返して終了
-  if (event.type !== 'message') {
-    //スタンプでもテキストでもなかったらnullを返す
-    if(event.message.type !== 'sticker' && event.message.type !== 'text') {
-      return Promise.resolve(null);
+    //メッセージでなかったらnullを返して終了
+    if (event.type !== 'message') {
+        //スタンプでもテキストでもなかったらnullを返す
+        if(event.message.type !== 'sticker' && event.message.type !== 'text') {
+            return Promise.resolve(null);
+        }
     }
-  }
 
-  if(event.message.type === 'text') {
+    if(event.message.type === 'text') {
     
-    let replyText = '';
+        let replyText = '';
 
-    //形態素解析の辞書の設定
-    const builder = kuromoji.builder({
-      dicPath: 'node_modules/kuromoji/dict/'
-    });
-
-
-    //形態素解析する関数
-    const analysis = async(err, tokenizer) => {
-
-        if(err) { throw err; }
-        await new Promise(resolve => {
-            let string = event.message.text;
-            let result = string.split(/\s+/);
-            let path = [];
-            // let replyText = '';
-            for(let i = 0; i < result.length; i++) {
-                path[i] = tokenizer.tokenize(result[i]);
-            }
-            for(let i = 0; i < path.length; i++) {
-                for(let j = 0; j < path[i].length; j++) {
-                    replyText += path[i][j].surface_form + '\t' +path[i][j].pos+ ':' +path[i][j].pos_detail_1+'\n';
-                }
-            }
-            console.log(replyText + 'ビルド部分');
-            resolve();
+        //形態素解析の辞書の設定
+        const builder = kuromoji.builder({
+            dicPath: 'node_modules/kuromoji/dict/'
         });
-        reply();
 
-    };
-    // これをどうにかする(実行する関数?)
-    builder.build(analysis);
-      //返信する
-      const reply = () => {
+        const exec = async() => {
+            await new Promise(resolve => {
+                builder.build((err, tokenizer) => {
+                    if(err) { throw err; }
+                    let string = event.message.text;
+                    let result = string.split(/\s+/);
+                    let path = [];
+                    // let replyText = '';
+                    for(let i = 0; i < result.length; i++) {
+                        path[i] = tokenizer.tokenize(result[i]);
+                    }
+                    for(let i = 0; i < path.length; i++) {
+                        for(let j = 0; j < path[i].length; j++) {
+                            replyText += path[i][j].surface_form + '\t' +path[i][j].pos+ ':' +path[i][j].pos_detail_1+'\n';
+                        }
+                    }
+                    resolve();
+                });
+            });
+            await new Promise(resolve => {
+                return client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: replyText,
+                });
+            });
+        };
+        exec();
 
-          console.log(replyText + '返信部分');
-          return client.replyMessage(event.replyToken, {
-              type: 'text',
-              text: replyText,
-          });
-
-      };
-
-    //Promisで非同期処理が終わるまでawitする
-
-    //ここまで
-  }
+        //ここまで
+    }
 
 
-  if(event.message.type === 'sticker') {
-    return client.replyMessage(event.replyToken, {
-      type: 'sticker',
-      packageId: 2,
-      stickerId: 163,
-    });
-  }
+    if(event.message.type === 'sticker') {
+        return client.replyMessage(event.replyToken, {
+            type: 'sticker',
+            packageId: 2,
+            stickerId: 163,
+        });
+    }
 
 }
 
